e9f47c53d4a20335da52246b22d8442ef96a842a
Aleksey Morozov <agres2iv@hotmail.com>
2025-05-07 16:06:38 +0300

    Test local

---
+++ src/main/java/com/example/demo/CalorieCalculationAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalorieCalculationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalorieCalculationAppApplication.class, args);
+	}
+
+}
