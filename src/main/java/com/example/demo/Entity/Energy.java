035505c9543058582540224c6ac26f426b2e48ad
Aleksey Morozov <agres2iv@hotmail.com>
2025-05-15 19:10:56 +0300

    Добавил сущностей

---
+++ src/main/java/com/example/demo/Entity/Energy.java
@@ -0,0 +1,60 @@
+package com.example.demo.Entity;
+
+public class Energy {
+    private int id;
+    private double protein;
+    private double fat;
+    private double carbs;
+    private double fibers;
+    private double calories;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    public double getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(double carbs) {
+        this.carbs = carbs;
+    }
+
+    public double getFibers() {
+        return fibers;
+    }
+
+    public void setFibers(double fibers) {
+        this.fibers = fibers;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+}
+
+
